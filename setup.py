d84811f82c1ea9667e8de4e5fd87164b3810932b
tiann01 <tianss1230@gmail.com>
2025-08-19 15:36:24 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ci-python-demo",
+    version="0.1.0",
+    packages=find_packages(),
+    py_modules=["main"],
+    install_requires=[],
+)
